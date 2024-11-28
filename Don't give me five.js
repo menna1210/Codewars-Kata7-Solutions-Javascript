01cd5ec32263e88024ade25c6d8a469c782f5ec8
menna1210 <mennaassem1210@gmail.com>
2024-11-28 13:31:06 +0200

    new commit

---
+++ Don't give me five.js
@@ -0,0 +1,15 @@
+// Don't give me five
+
+// https://www.codewars.com/kata/5813d19765d81c592200001a/train/javascript
+
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+function dontGiveMeFive(start,end){
+ let count =0;
+ for(let i=start;i<=end ;i++){
+    if(!String(i).includes('5')){count++;}
+   
+ }
+ return count;
+}
+console.log(dontGiveMeFive(1, 9));
